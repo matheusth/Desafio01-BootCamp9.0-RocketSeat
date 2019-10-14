e3514b6354c69b4df37606be83e6f3b5a96319cf
Matheus Henrique Passos <matheushtip@gmail.com>
2019-10-14 19:52:15 -0300

    Funçoes de adcionar tarefas e deletar projeto

--- routes.js
+++ routes.js
@@ -11,8 +11,21 @@ router.get("/projects", (req, res) => {
 });
 router.put("/projects/:id", (req, res) => {
   const { id } = req.params;
-  projects[id].title = req.body.title;
-  return res.json(projects[id]);
+  projects.find(value => value.id == id).title = req.body.title;
+  return res.json(projects);
 });
 
+router.delete("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  index = projects.findIndex(element => element.id == id);
+  projects.splice(index, 1);
+  return res.json(projects);
+});
+
+router.post("/projects/:id/tasks", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  projects.find(element => element.id == id).tasks.push(title);
+  return res.json(projects[id]);
+});
 module.exports = router;
