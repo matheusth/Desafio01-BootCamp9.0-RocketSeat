8b6d987e41f35d8b51723df2c6ad30e2a98dced4
Matheus Henrique Passos <matheushtip@gmail.com>
2019-10-14 20:14:18 -0300

    Desafio finalizado

--- routes.js
+++ routes.js
@@ -1,5 +1,17 @@
 const router = require("express").Router();
 const projects = [];
+/*
+  Middleware that checks if the project id exists to create a task,
+  delete or edit a project.
+*/
+function checkProject(req, res, next) {
+  const { id } = req.params;
+  if (!projects.find(element => element.id == id)) {
+    return res.status(400).json({ error: "Project does not exists" });
+  } else {
+    next();
+  }
+}
 
 router.post("/projects", (req, res) => {
   const { id, title } = req.body;
@@ -9,23 +21,23 @@ router.post("/projects", (req, res) => {
 router.get("/projects", (req, res) => {
   return res.json(projects);
 });
-router.put("/projects/:id", (req, res) => {
+router.put("/projects/:id", checkProject, (req, res) => {
   const { id } = req.params;
   projects.find(value => value.id == id).title = req.body.title;
   return res.json(projects);
 });
 
-router.delete("/projects/:id", (req, res) => {
+router.delete("/projects/:id", checkProject, (req, res) => {
   const { id } = req.params;
   index = projects.findIndex(element => element.id == id);
   projects.splice(index, 1);
   return res.json(projects);
 });
 
-router.post("/projects/:id/tasks", (req, res) => {
+router.post("/projects/:id/tasks", checkProject, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
   projects.find(element => element.id == id).tasks.push(title);
-  return res.json(projects[id]);
+  return res.json(projects);
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const routes = require("./routes");
 const app = express();
+var numReq = 0;
+/*This is a global Middleware that count the number of requests*/
+function countRequests(req, res, next) {
+  console.log(++numReq);
+  next();
+}
+
+app.use(countRequests);
 app.use(express.json());
 app.use(routes);
 app.listen(3000);
